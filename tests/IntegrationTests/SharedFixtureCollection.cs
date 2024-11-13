9ac4ae20f72c68bb5cc0611a1a264b4146a52016
Duke <commits@dmail.icu>
2024-11-13 01:13:22 +0100

    delete test projects

--- tests/IntegrationTests/SharedFixtureCollection.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace IntegrationTests;
-
-[CollectionDefinition("SharedFixture")]
-[SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "xUnit convention")]
-public sealed class SharedFixtureCollection : ICollectionFixture<SharedFixture>;

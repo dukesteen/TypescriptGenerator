238434206f8687f8f01253d40a9a44baedec0f3b
Duke <commits@dmail.icu>
2025-12-17 18:28:49 +0100

    make packable as tool

---
+++ justfile
@@ -0,0 +1,4 @@
+install-new-version:
+	dotnet pack src/TypescriptGenerator.Console/TypescriptGenerator.Console.csproj -c Release
+	dotnet tool uninstall -g Duke.TypescriptGenerator
+	dotnet tool install -g Duke.TypescriptGenerator --version "0.1.0-preview.0.25" --add-source /Users/duke/Dev/Personal/TypescriptGenerator/src/TypescriptGenerator.Console/nupkg
--- src/TypescriptGenerator.Console/Program.cs
+++ src/TypescriptGenerator.Console/Program.cs
@@ -1,10 +1,11 @@
 global using System;
+using System.Diagnostics;
 using System.Text.Json;
 
 using Cocona;
 
 using Microsoft.Build.Locator;
-using Microsoft.Extensions.Configuration;
+using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -19,10 +20,9 @@ MSBuildLocator.RegisterDefaults();
 
 var builder = CoconaApp.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("appsettings.json", false, true);
-
 var loggerConfiguration = new LoggerConfiguration()
-	.ReadFrom.Configuration(builder.Configuration)
+	.MinimumLevel.Information()
+	.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}")
 	.CreateLogger();
 
 Log.Logger = loggerConfiguration;
@@ -37,6 +37,9 @@ app.AddCommand("generate", async (
 	[FromService] ILogger<Generator> logger,
 	[FromService] IServiceProvider serviceProvider) =>
 {
+	var totalStopwatch = Stopwatch.StartNew();
+	var stepStopwatch = Stopwatch.StartNew();
+
 	var configText = await File.ReadAllTextAsync(configPath);
 	var config = JsonSerializer.Deserialize<GeneratorConfig>(configText);
 
@@ -46,11 +49,52 @@ app.AddCommand("generate", async (
 		return 2;
 	}
 
-	var policyGenerator = ActivatorUtilities.CreateInstance<PoliciesGenerator>(serviceProvider, config);
-	var result = await policyGenerator.Execute();
+	// Open project ONCE
+	var properties = new Dictionary<string, string>
+	{
+		{ "DesignTimeBuild", "true" },
+		{ "BuildProjectReferences", "false" },
+		{ "SkipCompilerExecution", "true" },
+		{ "RunAnalyzers", "false" },
+		{ "RunAnalyzersDuringBuild", "false" },
+	};
 
+	using var workspace = MSBuildWorkspace.Create(properties);
+	workspace.LoadMetadataForReferencedProjects = true;
+
+	logger.LogInformation("Opening project...");
+	stepStopwatch.Restart();
+	var project = await workspace.OpenProjectAsync(config.CsprojPath);
+	logger.LogInformation("Opened project in {ElapsedMs}ms", stepStopwatch.ElapsedMilliseconds);
+
+	logger.LogInformation("Getting compilation...");
+	stepStopwatch.Restart();
+	var compilation = await project.GetCompilationAsync()
+		?? throw new InvalidOperationException("Failed to get compilation");
+	logger.LogInformation("Got compilation in {ElapsedMs}ms", stepStopwatch.ElapsedMilliseconds);
+
+	var errorDiagnostics = compilation.GetDiagnostics().Where(x => x.WarningLevel == 0).ToList();
+	if (errorDiagnostics.Count != 0)
+	{
+		foreach (var errorDiagnostic in errorDiagnostics)
+			logger.LogError("{Message} @ {Location}", errorDiagnostic.GetMessage(), errorDiagnostic.Location.GetLineSpan());
+		return 1;
+	}
+
+	// Pass compilation to both generators
+	logger.LogInformation("Running policies generator...");
+	stepStopwatch.Restart();
+	var policyGenerator = ActivatorUtilities.CreateInstance<PoliciesGenerator>(serviceProvider, config);
+	var result = await policyGenerator.Execute(compilation);
+	logger.LogInformation("Policies generator completed in {ElapsedMs}ms", stepStopwatch.ElapsedMilliseconds);
+
+	logger.LogInformation("Running TypeScript generator...");
+	stepStopwatch.Restart();
 	var generator = ActivatorUtilities.CreateInstance<Generator>(serviceProvider, config);
-	result = await generator.Execute();
+	result = await generator.Execute(compilation);
+	logger.LogInformation("TypeScript generator completed in {ElapsedMs}ms", stepStopwatch.ElapsedMilliseconds);
+
+	logger.LogInformation("Total time: {ElapsedMs}ms", totalStopwatch.ElapsedMilliseconds);
 	return result;
 });
 
